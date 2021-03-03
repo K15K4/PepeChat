2190ae60bf7346b6b49a28618c9eb2a6fce46ef2
K15K4 <zyonion9972@gmail.com>
2021-03-03 13:33:21 +0300

    Добавьте файлы проекта.

---
+++ SuppaChat/LoginWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SuppaChat
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Icon = new BitmapImage(new Uri("pack://application:,,,/Resources/4.png"));
+            ShowPepe.MouseUp += new MouseButtonEventHandler(ShowPepe_MouseUp);
+            ShowPepe.MouseDown += new MouseButtonEventHandler(ShowPepe_MouseDown);
+        }
+
+        private void ShowPepe_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPepe.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/TripleMonkas.png"));
+        }
+
+        private void ShowPepe_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ShowPepe.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/TripleMonkasSleep.png"));
+        }
+
+        private void RegButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow window = new RegistrationWindow();
+            this.Hide();
+            window.Show();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
---
+++ SuppaChat/RegistrationWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SuppaChat
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Icon = new BitmapImage(new Uri("pack://application:,,,/Resources/4.png"));
+            ShowPepe1.MouseUp += new MouseButtonEventHandler(ShowPepe1_MouseUp);
+            ShowPepe1.MouseDown += new MouseButtonEventHandler(ShowPepe1_MouseDown);
+            ShowPepe2.MouseUp += new MouseButtonEventHandler(ShowPepe2_MouseUp);
+            ShowPepe2.MouseDown += new MouseButtonEventHandler(ShowPepe2_MouseDown);
+        }
+
+        private void ShowPepe1_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ShowPepe1.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/TripleMonkasSleep.png"));
+        }
+
+        private void ShowPepe1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPepe1.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/TripleMonkas.png")); 
+        }
+        private void ShowPepe2_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ShowPepe2.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/TripleMonkasSleep.png"));
+        }
+
+        private void ShowPepe2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPepe2.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/TripleMonkas.png"));
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow window = new LoginWindow();
+            this.Hide();
+            window.Show();
+        }
+    }
+}
